f5d0fbcd630352bab3e4f3918dd1bebc467529ed
dinethde <dinethran33@gmail.com>
2025-11-18 09:10:48 +0530

    fix: add check for isAuthenticated before retrieving access token in refreshToken function

--- webapps/webapp-template/src/context/AuthContext.tsx
+++ webapps/webapp-template/src/context/AuthContext.tsx
@@ -142,6 +142,11 @@ const AppAuthProvider = (props: { children: React.ReactNode }) => {
   }, [state.isAuthenticated, state.isLoading]);
 
   const refreshToken = async (): Promise<{ accessToken: string }> => {  
+    if (state.isAuthenticated) {
+      const accessToken = await getIDToken();
+      return {accessToken}
+    }
+
     try {  
       await refreshAccessToken();  
       const accessToken = await getAccessToken();  
